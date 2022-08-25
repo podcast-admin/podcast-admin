971a25dbd647f2066724f061a901d461b3c78b27
Jonas Piela <jonas.piela@gmail.com>
2022-08-25 07:15:08 +0000

    WIP Adds list of invites

--- src/containers/Users/UsersList.js
+++ src/containers/Users/UsersList.js
@@ -1,45 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { httpsCallable } from 'firebase/functions';
-import { functions } from '../../helpers/Firebase';
 
-import {
-  Container,
-  Stack,
-  Button,
-  Paper,
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Avatar,
-  CircularProgress,
-} from '@mui/material';
+import { Container, Stack, Button, Typography } from '@mui/material';
+
+import ListOfUsers from './components/ListOfUsers';
+import ListOfInvites from './components/ListOfInvites';
 
 const UsersList = () => {
   const [t] = useTranslation();
-  const { podcastId } = useParams();
-  const [users, setUsers] = useState([]);
-  const usersList = httpsCallable(functions, 'users-list');
-
-  useEffect(() => {
-    const getUsers = async () => {
-      usersList({ podcastId })
-        .then((result) => {
-          setUsers(result.data);
-        })
-        .catch((e) => {
-          // TODO: Handle error
-        });
-    };
-
-    getUsers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <Container
@@ -54,76 +22,13 @@ const UsersList = () => {
         </Typography>
         <Button href="users/invite">{t('UsersList.inviteButtonLabel')}</Button>
       </Stack>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>&nbsp;</TableCell>
-              <TableCell>{t('UsersList.displayName')}</TableCell>
-              <TableCell>{t('UsersList.email')}</TableCell>
-              <TableCell>{t('UsersList.creationTime')}</TableCell>
-              <TableCell>{t('UsersList.lastSignInTime')}</TableCell>
-              <TableCell>{t('UsersList.providerIds')}</TableCell>
-              <TableCell>&nbsp;</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.length > 0 ? (
-              users.map((user) => (
-                <TableRow
-                  key={user.displayName}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell sx={{ width: '40px' }}>
-                    <Avatar src={user.photoURL} />
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {user.displayName}
-                  </TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>
-                    {t('UsersList.intlDateTime', {
-                      val: Date.parse(user.creationTime),
-                      formatParams: {
-                        val: {
-                          weekday: 'short',
-                          year: 'numeric',
-                          month: 'long',
-                          day: 'numeric',
-                          hour: 'numeric',
-                          minute: 'numeric',
-                        },
-                      },
-                    })}
-                  </TableCell>
-                  <TableCell>
-                    {t('UsersList.intlDateTime', {
-                      val: Date.parse(user.lastSignInTime),
-                      formatParams: {
-                        val: {
-                          weekday: 'short',
-                          year: 'numeric',
-                          month: 'long',
-                          day: 'numeric',
-                          hour: 'numeric',
-                          minute: 'numeric',
-                        },
-                      },
-                    })}
-                  </TableCell>
-                  <TableCell>{user.providerIds.join(', ')}</TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell align="center" colSpan={7}>
-                  <CircularProgress />
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+
+      <ListOfUsers />
+
+      <Typography variant="h3" gutterBottom mt={5}>
+        {t('UsersList.title')}
+      </Typography>
+      <ListOfInvites />
     </Container>
   );
 };
---
+++ src/containers/Users/components/ListOfInvites.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { doc, getDoc } from 'firebase/firestore';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  CircularProgress,
+} from '@mui/material';
+import { db } from '../../../helpers/Firebase';
+
+const ListOfInvites = () => {
+  const [t] = useTranslation();
+  const [invites, setInvites] = useState([]);
+  const { podcastId } = useParams();
+
+  useEffect(() => {
+    const getInvites = async () => {
+      getDoc(doc(db, `podcasts/${podcastId}`))
+        .then((result) => {
+          setInvites(result.data().invitedAdmins || []);
+        })
+        .catch((e) => {
+          // TODO: Handle error
+        });
+    };
+
+    getInvites();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('ListOfInvites.email')}</TableCell>
+            <TableCell>{t('ListOfInvites.createdAt')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {invites.length > 0 ? (
+            invites.map((invite) => (
+              <TableRow
+                key={invite.email}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{invite.email}</TableCell>
+                <TableCell>
+                  {t('UsersList.intlDateTime', {
+                    val: Date.parse(invite.createdAt.toDate()),
+                    formatParams: {
+                      val: {
+                        weekday: 'short',
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                      },
+                    },
+                  })}
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell align="center" colSpan={7}>
+                <CircularProgress />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default ListOfInvites;
