c433b511caf522f6a43a7ab2b95fc2cfe13cdb60
Jonas Piela <jonas.piela@gmail.com>
2022-09-02 15:04:03 +0000

    Refactors Item

--- src/components/episodes/Item/Item.js
+++ src/components/episodes/Item/Item.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import LazyLoad from 'react-lazyload';
 import ReactMarkdown from 'react-markdown';
@@ -22,25 +22,24 @@ import { db } from '../../../helpers/Firebase';
 import { doc, updateDoc } from 'firebase/firestore';
 import { Timestamp } from 'firebase/firestore';
 
-class Item extends Component {
-  reprocessAudio = async () => {
-    const { podcastId, episodeId } = this.props;
-
+const Item = ({
+  podcastId,
+  episodeId,
+  item: { date, title, subtitle, image, description, processing },
+}) => {
+  const reprocessAudio = async () => {
     await updateDoc(doc(db, 'podcasts', podcastId, 'episodes', episodeId), {
       processing: 'restart',
     });
   };
 
-  renderProcessingStatusButton = () => {
-    const { item } = this.props;
-    if (item.processing === 'done') {
+  const renderProcessingStatusButton = () => {
+    if (processing === 'done') {
       return (
         <Tooltip title="Audio-Datei neu erstellen">
           <IconButton
             aria-label="reprocess"
-            onClick={() => {
-              this.reprocessAudio();
-            }}
+            onClick={reprocessAudio}
             size="large"
           >
             <ReplayIcon />
@@ -48,14 +47,12 @@ class Item extends Component {
         </Tooltip>
       );
     }
-    if (item.processing === 'restart') {
+    if (processing === 'restart') {
       return (
         <Tooltip title="Audio-Datei wird neu erstellt...">
           <IconButton
             aria-label="reprocess"
-            onClick={() => {
-              this.reprocessAudio();
-            }}
+            onClick={reprocessAudio}
             size="large"
           >
             <HourglassEmptyIcon />
@@ -66,52 +63,45 @@ class Item extends Component {
     return false;
   };
 
-  render() {
-    const {
-      item: { date, title, subtitle, image, description },
-      episodeId,
-    } = this.props;
-
-    return (
-      <Paper sx={{ padding: 2, marginBottom: 2 }}>
-        <Stack spacing={0} mb={2}>
-          <Typography variant="subtitle1">
-            <Moment format="DD.MM.YYYY">{date.toDate()}</Moment>
-          </Typography>
-          <Typography variant="h4">{title}</Typography>
-          <Typography variant="subtitle1">{subtitle}</Typography>
-        </Stack>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <LazyLoad>
-              <Box
-                component="img"
-                sx={{ width: '100%' }}
-                src={image}
-                alt={title}
-              />
-            </LazyLoad>
-            <Tooltip title="Episode bearbeiten">
-              <IconButton
-                aria-label="edit"
-                href={`episodes/${episodeId}/edit`}
-                size="large"
-              >
-                <EditIcon />
-              </IconButton>
-            </Tooltip>
-            {this.renderProcessingStatusButton()}
-          </Grid>
-          <Grid item xs={9}>
-            <Typography variant="body1" component="div">
-              <ReactMarkdown linkTarget="_blank">{description}</ReactMarkdown>
-            </Typography>
-          </Grid>
+  return (
+    <Paper sx={{ padding: 2, marginBottom: 2 }}>
+      <Stack spacing={0} mb={2}>
+        <Typography variant="subtitle1">
+          <Moment format="DD.MM.YYYY">{date.toDate()}</Moment>
+        </Typography>
+        <Typography variant="h4">{title}</Typography>
+        <Typography variant="subtitle1">{subtitle}</Typography>
+      </Stack>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <LazyLoad>
+            <Box
+              component="img"
+              sx={{ width: '100%' }}
+              src={image}
+              alt={title}
+            />
+          </LazyLoad>
+          <Tooltip title="Episode bearbeiten">
+            <IconButton
+              aria-label="edit"
+              href={`episodes/${episodeId}/edit`}
+              size="large"
+            >
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          {renderProcessingStatusButton()}
         </Grid>
-      </Paper>
-    );
-  }
-}
+        <Grid item xs={9}>
+          <Typography variant="body1" component="div">
+            <ReactMarkdown linkTarget="_blank">{description}</ReactMarkdown>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 Item.propTypes = {
   item: PropTypes.shape({
