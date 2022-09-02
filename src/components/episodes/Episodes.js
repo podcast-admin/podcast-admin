52f267bb1bb7a9b82c40d0143f562a45d6770b87
Jonas Piela <jonas.piela@gmail.com>
2022-09-02 14:40:20 +0000

    Fixes constant refreshing of episode list

--- src/components/episodes/Episodes.js
+++ src/components/episodes/Episodes.js
@@ -1,34 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import {
-  getFirestore,
-  collection,
-  query,
-  orderBy,
-  onSnapshot,
-} from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 import { Typography, Link } from '@mui/material';
-import firebaseApp from '../../helpers/Firebase';
+import { db } from '../../helpers/Firebase';
 
 import Item from './Item';
 
-const Episodes = (props) => {
+const Episodes = () => {
   const [t] = useTranslation();
   const { podcastId } = useParams();
   const [state, setState] = useState({
     episodes: [],
   });
 
-  const db = getFirestore(firebaseApp);
-  const episodesRef = collection(db, 'podcasts', podcastId, 'episodes');
-  const q = query(episodesRef, orderBy('date', 'desc'));
+  useEffect(() => {
+    const episodesRef = collection(db, 'podcasts', podcastId, 'episodes');
+    const q = query(episodesRef, orderBy('date', 'desc'));
 
-  onSnapshot(q, (snapshot) => {
-    setState({
-      episodes: snapshot.docs,
+    return onSnapshot(q, (snapshot) => {
+      setState({
+        episodes: snapshot.docs,
+      });
     });
-  });
+  }, [podcastId]);
 
   return (
     <>
