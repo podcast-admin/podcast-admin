4f62737c387a539cf927243e6f3f19705f69fa61
Jonas Piela <jonas.piela@gmail.com>
2023-04-16 13:27:54 +0000

    Uses useQuery to fetch analytics data

--- src/containers/Analytics/Analytics.js
+++ src/containers/Analytics/Analytics.js
@@ -1,36 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { httpsCallable } from 'firebase/functions';
 import withAuth from '../../helpers/WithAuth';
-import { functions, auth } from '../../helpers/Firebase';
+import useTotalDownloadsQuery from '../../hooks/useTotalDownloadsQuery';
 import BarChart from './components/BarChart';
-import { Container, Paper, Typography } from '@mui/material';
+import { Container, Paper, Typography, CircularProgress } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 const Analytics = () => {
-  const [data, setData] = useState([]);
   const [t] = useTranslation();
   const { podcastId } = useParams();
-
-  useEffect(() => {
-    const getData = async () => {
-      const getTotalDownloads = httpsCallable(
-        functions,
-        'analytics-totalDownloads',
-      );
-
-      getTotalDownloads({
-        podcastId,
-      })
-        .then(({ data }) => {
-          setData(data.slice(0, 10));
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    };
-    getData();
-  }, [podcastId]);
+  const { data, isLoading, isSuccess } = useTotalDownloadsQuery(podcastId);
 
   return (
     <Container maxWidth="lg" sx={{ paddingY: 2 }}>
@@ -38,7 +16,8 @@ const Analytics = () => {
         <Typography variant="h4" gutterBottom>
           {t('Analytics.title')}
         </Typography>
-        <BarChart data={data} />
+        {isLoading && <CircularProgress />}
+        {isSuccess && <BarChart data={data.slice(0, 10)} />}
       </Paper>
     </Container>
   );
