00cccfee81c66dbaa1c79d53f8b2f96bfad09076
Jonas Piela <jonas.piela@gmail.com>
2024-02-17 07:22:16 +0000

    WIP First transcriptions did work

--- functions/lib/transcribeAudioFile.js
+++ functions/lib/transcribeAudioFile.js
@@ -1,3 +1,4 @@
+const speech = require('@google-cloud/speech');
 const pathToFfmpeg = require('ffmpeg-static');
 const { path: pathToFfprobe } = require('ffprobe-static');
 const admin = require('firebase-admin');
@@ -5,9 +6,10 @@ const { onRequest } = require('firebase-functions/v2/https');
 const ffmpeg = require('fluent-ffmpeg');
 const os = require('os');
 const path = require('path');
-const speech = require('@google-cloud/speech');
 
-const client = new speech.SpeechClient();
+const BUCKET = 'podcast-admin.appspot.com';
+const FILE =
+  'podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/digitale-services-in-2024/original-audio-1708153834380.wav';
 
 const downloadFile = async (fileBucket, filePath) => {
   const fileName = path.basename(filePath);
@@ -21,11 +23,13 @@ const downloadFile = async (fileBucket, filePath) => {
   return tempFilePath;
 };
 
-const getAudioMetadata = async () => {
-  const audioFilePath = await downloadFile(
-    'podcast-admin.appspot.com',
-    '/podcasts/my-podcast/episodes/mytestwisdom/original-audio-1657635984929.mp3',
-  );
+/**
+ *
+ * @param {String} fileName
+ * @return {Promise<String>}
+ */
+const getAudioMetadata = async (fileName) => {
+  const audioFilePath = await downloadFile(BUCKET, fileName);
 
   return new Promise((resolve, reject) => {
     ffmpeg()
@@ -43,7 +47,60 @@ const getAudioMetadata = async () => {
   });
 };
 
+/**
+ *
+ * @param {String} gcsUri
+ * @param {String} encoding
+ * @param {String} sampleRateHertz
+ */
+const transcribeAudio = async (gcsUri, encoding, sampleRateHertz) => {
+  // https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig#audioencoding
+  const languageCode = 'de-DE';
+
+  const client = new speech.SpeechClient();
+
+  const config = {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  };
+
+  const audio = {
+    uri: gcsUri,
+  };
+
+  const request = {
+    config: config,
+    audio: audio,
+    outputConfig: {
+      gcsUri: `gs://${BUCKET}/podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/digitale-services-in-2024/transcript.json`,
+    },
+  };
+
+  return await client.longRunningRecognize(request); // Not sure if it works... we need to list running processes
+};
+
+const getEncoding = (codec) => {
+  switch (codec) {
+    case 'pcm_s24le':
+      return 'LINEAR16';
+    case 'mp3':
+      return 'MP3';
+    default:
+      return 'ENCODING_UNSPECIFIED';
+  }
+};
+
 module.exports = onRequest(async (req, res) => {
-  const result = await getAudioMetadata();
-  res.send(result);
+  try {
+    const { codec_name, sample_rate } = await getAudioMetadata(FILE);
+    const result = await transcribeAudio(
+      `gs://${BUCKET}/${FILE}`,
+      getEncoding(codec_name),
+      sample_rate,
+    );
+    res.send(result);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
