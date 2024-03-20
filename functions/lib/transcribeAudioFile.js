67e86ab40c06a4f310ae7ba17a76585b3cff7776
Jonas Piela <jonas.piela@gmail.com>
2024-03-20 20:52:25 +0000

    It works with some audio files

--- functions/lib/transcribeAudioFile.js
+++ functions/lib/transcribeAudioFile.js
@@ -1,55 +1,15 @@
 const { SpeechClient } = require('@google-cloud/speech').v2;
-const pathToFfmpeg = require('ffmpeg-static');
-const { path: pathToFfprobe } = require('ffprobe-static');
-const admin = require('firebase-admin');
 const { onRequest } = require('firebase-functions/v2/https');
-const ffmpeg = require('fluent-ffmpeg');
-const os = require('os');
-const path = require('path');
 
 const BUCKET = 'podcast-admin.appspot.com';
+
+// It doesn't work with some WAV files that were created with Garage Band.
 const FILE =
-  'podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/digitale-services-in-2024/original-audio-1708153834380.wav';
+  'podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/8-prinzipien-zur-entwicklung-digitaler-services/original-audio-1687337093368.mp3';
 
 const client = new SpeechClient({
   apiEndpoint: 'europe-west4-speech.googleapis.com',
 });
-
-const downloadFile = async (fileBucket, filePath) => {
-  const fileName = path.basename(filePath);
-  const bucket = admin.storage().bucket(fileBucket);
-  const tempFilePath = path.join(os.tmpdir(), fileName);
-
-  await bucket
-    .file(filePath)
-    .download({ destination: tempFilePath, validation: false });
-
-  return tempFilePath;
-};
-
-/**
- *
- * @param {String} fileName
- * @return {Promise<String>}
- */
-const getAudioMetadata = async (fileName) => {
-  const audioFilePath = await downloadFile(BUCKET, fileName);
-
-  return new Promise((resolve, reject) => {
-    ffmpeg()
-      .setFfmpegPath(pathToFfmpeg)
-      .setFfprobePath(pathToFfprobe)
-      .input(audioFilePath)
-      .ffprobe((err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          const { codec_name, sample_rate } = data.streams[0];
-          resolve({ codec_name, sample_rate });
-        }
-      });
-  });
-};
 
 /**
  *
@@ -57,50 +17,33 @@ const getAudioMetadata = async (fileName) => {
  * @param {String} encoding
  * @param {String} sampleRateHertz
  */
-const transcribeAudio = async (gcsUri, encoding, sampleRateHertz) => {
-  // https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig#audioencoding
-  const languageCode = 'de-DE';
-
+const transcribeAudio = async (gcsUri) => {
+  /**
+   * See more config options here
+   * https://googleapis.dev/nodejs/speech/latest/google.cloud.speech.v2.IBatchRecognizeRequest.html
+   */
   const request = {
     recognizer:
       'projects/podcast-admin/locations/europe-west4/recognizers/podcast-de-chirp',
     config: {
-      // encoding: encoding,
-      // sampleRateHertz: sampleRateHertz,
-      autoDecodingConfig: {},
+      model: 'chirp',
     },
     files: [{ uri: gcsUri }],
     recognitionOutputConfig: {
       gcsOutputConfig: {
-        uri: `gs://${BUCKET}/podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/digitale-services-in-2024/transcript.json`,
+        uri: `gs://${BUCKET}/${FILE.split('/').slice(0, -1).join('/')}`,
       },
     },
   };
 
-  const [operation] = await client.batchRecognize(request); // Not sure if it works... we need to list running processes
+  const [operation] = await client.batchRecognize(request);
   const [response] = await operation.promise();
   return response;
 };
 
-const getEncoding = (codec) => {
-  switch (codec) {
-    case 'pcm_s24le':
-      return 'LINEAR16';
-    case 'mp3':
-      return 'MP3';
-    default:
-      return 'ENCODING_UNSPECIFIED';
-  }
-};
-
 exports.run = onRequest(async (req, res) => {
   try {
-    const { codec_name, sample_rate } = await getAudioMetadata(FILE);
-    const result = await transcribeAudio(
-      `gs://${BUCKET}/${FILE}`,
-      getEncoding(codec_name),
-      sample_rate,
-    );
+    const result = await transcribeAudio(`gs://${BUCKET}/${FILE}`);
     res.send(result);
   } catch (e) {
     res.status(500).send(e);
