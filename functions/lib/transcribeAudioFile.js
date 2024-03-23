eb91714a4bcb539e70f15422d97111109d15e9fd
Jonas Piela <jonas.piela@gmail.com>
2024-03-23 11:15:19 +0000

    Triggers transcription from ui

--- functions/lib/transcribeAudioFile.js
+++ functions/lib/transcribeAudioFile.js
@@ -7,16 +7,13 @@ const {
 } = require('firebase-functions');
 const { onCall } = require('firebase-functions/v2/https');
 const { onMessagePublished } = require('firebase-functions/v2/pubsub');
+const yup = require('yup');
 
 const BUCKET = 'podcast-admin.appspot.com';
 const PUBSUB_TOPIC = 'elearning-report';
 
 const db = getFirestore();
 const collectionRef = db.collection('podcasts');
-
-// It doesn't work with some WAV files that were created with Garage Band.
-const FILE =
-  'podcasts/2T9aVqEJhvd1Fck2vVPp/episodes/8-prinzipien-zur-entwicklung-digitaler-services/original-audio-1687337093368.mp3';
 
 const client = new SpeechClient({
   apiEndpoint: 'europe-west4-speech.googleapis.com',
@@ -42,7 +39,7 @@ const transcribeAudio = async (gcsUri) => {
     files: [{ uri: gcsUri }],
     recognitionOutputConfig: {
       gcsOutputConfig: {
-        uri: `gs://${BUCKET}/${FILE.split('/').slice(0, -1).join('/')}`,
+        uri: gcsUri.split('/').slice(0, -1).join('/'),
       },
     },
   };
@@ -55,6 +52,22 @@ const transcribeAudio = async (gcsUri) => {
 exports.uiEndpoint = onCall(
   (request) =>
     new Promise(async (resolve, reject) => {
+      const requestDataSchema = yup.object({
+        podcastId: yup.string().required(),
+        episodeId: yup.string().required(),
+      });
+
+      // Reject the call if the required data is not provided
+      try {
+        requestDataSchema.validateSync(request.data);
+      } catch (e) {
+        error('The user is not unauthenticated.', request);
+        throw new HttpsError('invalid-argument', e.message);
+      }
+
+      const podcastId = request.data.podcastId;
+      const episodeId = request.data.episodeId;
+
       // Reject the call if the user is not authenticated
       if (!request.auth) {
         error('The user is not unauthenticated.', request);
@@ -66,8 +79,8 @@ exports.uiEndpoint = onCall(
 
       const token = request.auth.token;
 
-      // Only admins are allowed to execute this operation
-      if (!token.admin) {
+      // Only owners of the podcast are allowed to execute this operation
+      if (!token.podcasts?.includes(podcastId)) {
         error('The user is not authorized to execute this operation.', request);
         throw new HttpsError(
           'permission-denied',
@@ -75,17 +88,15 @@ exports.uiEndpoint = onCall(
         );
       }
 
-      const podcastId = request.data.podcastId;
-      const episodeId = request.data.episodeId;
-
+      // Getting original audio file and updating firestore should happen in pubsub func
       const episodeDoc = collectionRef
         .doc(podcastId)
         .collection('episodes')
         .doc(episodeId);
       const episodeSnap = await episodeDoc.get();
-      const originalAudioPath = episodeSnap.data().original_audio;
+      const audioOriginalPath = episodeSnap.data().audio_original;
 
-      const data = Buffer.from(originalAudioPath);
+      const data = Buffer.from(audioOriginalPath);
       const pubSubClient = new PubSub();
       pubSubClient
         .topic(PUBSUB_TOPIC)
@@ -100,7 +111,7 @@ exports.pubsubEndpoint = onMessagePublished(
   (event) =>
     new Promise(async (resolve, reject) => {
       const gcsUri = atob(event.data.message.data);
-      transcribeAudio(gcsUri)
+      transcribeAudio(`gs://${BUCKET}/${gcsUri}`)
         .then((result) => {
           info(`Transcription of ${gcsUri} finished.`, result);
           resolve();
